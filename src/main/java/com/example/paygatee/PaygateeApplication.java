44316e0179ac1eb650ac66ccc1c8a066f393c8f9
SavvySahai <savvysahai6843@gmail.com>
2023-01-26 23:03:24 +0530

    Initial commit

---
+++ src/main/java/com/example/paygatee/PaygateeApplication.java
@@ -0,0 +1,13 @@
+package com.example.paygatee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaygateeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaygateeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paygatee/PaygateeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paygatee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaygateeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
